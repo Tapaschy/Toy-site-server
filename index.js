4cf47980917c137b955bc5edc5610eced2667c25
Tapas chowdhury <tapaschy12@gmail.com>
2023-05-19 23:32:15 +0600

    new data added

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ async function run() {
     })
 
 
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -54,6 +55,7 @@ async function run() {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 
